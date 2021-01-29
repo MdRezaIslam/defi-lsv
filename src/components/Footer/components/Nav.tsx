2d28e011199797a997cf70bd9c3b16252ee3120c
Md Reza Islam RS <52669170+MdRezaIslam@users.noreply.github.com>
2021-01-29 08:24:49 -0800

    add done

--- src/components/Footer/components/Nav.tsx
+++ src/components/Footer/components/Nav.tsx
@@ -16,67 +16,22 @@ const Nav: React.FC = () => {
       >
         LfiSwap LFI-ETH
       </StyledLink> */}
-      <StyledLink target="_blank" href="#">
+      <StyledLink target="_blank" href="http://wCww.t.me/LFIFinance">
         Telegram
-      </StyledLink>
-      <StyledLink target="_blank" href="http://www.t.me/LFIFinance">
-        Github
       </StyledLink>
       <StyledLink target="_blank" href="https://github.com/MdRezaIslam/defi-lsv">
-        Twitter
+        Github
       </StyledLink>
       <StyledLink target="_blank" href="http://www.twitter.com/lsvdefi">
-        Facebook
-      </StyledLink>
-	  <StyledLink target="_blank" href="http://www.facebook.com/lsvdefi.finance">
-		  )
-		  }
-		  
-const StyledNav = styled.nav`
-  align-items: center;
-  display: flex;
-`
-
-const StyledLink = styled.a`
-  color: ${(props) => props.theme.color.grey[400]};
-  padding-left: ${(props) => props.theme.spacing[3]}px;
-  padding-right: ${(props) => props.theme.spacing[3]}px;
-  text-decoration: none;
-  &:hover {V
-    color: ${(props) => props.theme.color.grey[500]};
-  }
-`
-
-export default Nav
-import React from 'react'
-import styled from 'styled-components'
-
-const Nav: React.FC = () => {
-  return (
-    <StyledNav>
-      <StyledLink
-        target="_blank"
-        href="#"
-      >
-        MasterChef Contract
-      </StyledLink>
-      {/*<StyledLink
-        target="_blank"
-        href="https://uniswap.info/pair/0xce84867c3c02b05dc570d0135103d3fb9cc19433"
-      >
-        LfiSwap LFI-ETH
-      </StyledLink> */}
-      <StyledLink target="_blank" href="#">
-        Telegram
-      </StyledLink>
-      <StyledLink target="_blank" href="http://www.t.me/LFIFinance">
-        Facebook
+        Twitter
       </StyledLink>
       <StyledLink target="_blank" href="http://www.facebook.com/lsvdefi.finance">
-        Twitter
+        Facebook
       </StyledLink>
-      <StyledLink target="_blank" href="http://www.twitter.com/lsvdefi">
-       
+	  <StyledLink target="_blank" href="http://www.t.me//lfinews">
+        Announcement
+      </StyledLink>
+    </StyledNav>
   )
 }
 
